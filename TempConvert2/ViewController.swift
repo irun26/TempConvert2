80baa02c8a18897d8437d90c65e096cb4c8bc68a
Jimson Vedua <jmsn133@gmail.com>
2018-02-11 12:13:18 -0500

    First Commit

--- TempConvert2/ViewController.swift
+++ TempConvert2/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextfield: UITextField!
+    @IBOutlet weak var greyLabel: UILabel!
+    
+    @IBAction func TestButtonYellow(_ sender: Any) {
+
+        if view.backgroundColor == #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) {
+            view.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            print("background is now green")
+            (sender as! UIButton).backgroundColor = UIColor.red
+            print("button is now red")
+            (sender as! UIButton).setTitleColor(.blue, for: .normal)
+            print("text is now blue")
+            
+            print("1 input textfield is \(String(describing: inputTextfield.text ?? nil))")
+            greyLabel.text = inputTextfield.text
+            
+        } else {
+            view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            print("background is now red")
+
+            (sender as! UIButton).backgroundColor = UIColor.green
+            print("button is now green")
+            (sender as! UIButton).setTitleColor(.yellow, for: .normal)
+            print("text is now yellow")
+
+            print("2 input textfield is \(String(describing: inputTextfield.text ?? nil))")
+
+            greyLabel.text = inputTextfield.text
+        }
+
+            
+            
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
